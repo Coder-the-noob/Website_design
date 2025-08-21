8647c4744a65b1b1a6354a065973d502ccaf3518
Coder-the-noob <moinmatubbar6@gmail.com>
2025-08-22 05:33:21 +0600

    added the font in config file and button color

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+      },
+    },
+  },
+}
+// This configuration file extends the default Tailwind CSS theme to include a custom font family "Manrope".
